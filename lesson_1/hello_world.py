57b4fcb0553b8bea0b6abbd30f4406928dde5499
Kerry Lee Hartley <khartley23@gmail.com>
2022-05-19 17:36:36 -0400

    Homework Submission 1

---
+++ lesson_1/hello_world.py
@@ -0,0 +1,9 @@
+
+
+random_name = 45.8
+
+print(random_name)
+
+random_name_float = int(random_name)
+print(random_name_float)  
+
--- lesson_1/homework_1_3.py
+++ lesson_1/homework_1_3.py
@@ -4,15 +4,21 @@ import math
 # Round a off to three decimal places.
 
 a = 10.04789
-result_1 = None
+result_1 = round(a, 3)
+print(result_1)
 
 
 # Round up result_2 to greater value.
 
 result_2 = 5 / 2 * 6 + 1.25 - 4
-
+print(result_2)
+result_2 = math.ceil(result_2)
+print(result_2)
 
 # Round up result_3 to lesser value.
 
 result_3 = 8 / 3 * 5 + 4.75 - 7
+print(result_3) 
+result_3 = math.floor(result_3)
+print(result_3) 
 
