5dd6d5f5a37c9f1346a52fe3f21a8daed77f08ea
Kerry Lee Hartley <khartley23@gmail.com>
2022-05-19 17:16:03 -0400

    Project Start

---
+++ Careerist Python Project/bar_exercise.py
@@ -0,0 +1,22 @@
+# Bar exercise (in the same file as before):
+# 1. create the variable with the name of your bar, 
+# create user input to enter the name of the guest
+
+MY_BAR_NAME = 'Nobu'
+GUEST_NAME = 'Kerry'
+
+#print(MY_BAR_NAME, GUEST_NAME)
+ 
+# 2. print the welcome  message "Welcome to the bar MY_BAR_NAME, GUEST_NAME" 
+# (MY_BAR_NAME is your bar name variable and the GUEST_NAME name is 
+# the value of the  respective variable)
+
+#print("Welcome to the bar",MY_BAR_NAME,",", GUEST_NAME)
+
+# 3. print the message with the statement, how many of each drink being sold
+# and the income you got today, for instance
+# "I have sold 15 beers and 20 wine glasses today,
+# and my income for today is 77 euros"
+
+print("Welcome to the bar",MY_BAR_NAME,",", GUEST_NAME,". I have sold 15 beers and 20 wine glasses today,"
+"and my income for today is 77 euros" )
